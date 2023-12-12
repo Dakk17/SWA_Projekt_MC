660ab27c2ca7d87da4b0cfec6e051b5e678b90c8
Dakk17 <131455281+Dakk17@users.noreply.github.com>
2023-12-12 22:28:20 +0100

    Add files via upload

--- Stránky/PHP/index.php
+++ Stránky/PHP/index.php
@@ -1,5 +1,4 @@
 <?php
-echo "Server API: " . php_sapi_name();
 
 $servername = "sql11.freesqldatabase.com";
 $username = "sql11669104";
@@ -14,20 +13,86 @@ if ($conn->connect_error) {
     die("Chyba připojení k databázi: " . $conn->connect_error);
 }
 
+// Získání IP adresy klienta
+$ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
+
+// Pokud je adresa ::1, použij localhost
+if ($ipAddress == '::1') {
+    $ipAddress = '127.0.0.1';
+}
+
+// Konverze IPv6 na IPv4
+$ipAddress = filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+
 // Nastavení časové zóny v rámci PHP
 date_default_timezone_set('Europe/Prague'); // Změňte na svou potřebnou časovou zónu
 
 // Zpracování formuláře pro registraci nebo čtení/zápis dat
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include "registrace.php"; // Skript pro registraci, pokud byl odeslán formulář
-} elseif (isset($_GET["action"]) && $_GET["action"] == "read") {
-    include "read_data.php"; // Skript pro čtení dat, pokud je zadána akce "read"
-} elseif (isset($_GET["action"]) && $_GET["action"] == "write") {
-    include "write_data.php"; // Skript pro zápis dat, pokud je zadána akce "write"
+    // Získání dat z formuláře
+    $realname = $conn->real_escape_string($_POST["realname"]);
+    $name = strtolower($realname);
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $email = isset($_POST["email"]) && $_POST["email"] !== "" ? $conn->real_escape_string($_POST["email"]) : null;
+
+    // Kontrola, zda uživatel s daným jménem nebo emailem již existuje
+    if ($email !== null) {
+        $checkQuery = "SELECT * FROM users WHERE LOWER(name) = LOWER('$name') OR email = '$email'";
+    } else {
+        $checkQuery = "SELECT * FROM users WHERE LOWER(name) = LOWER('$name') AND email =!";
+    }
+    
+    $checkResult = $conn->query($checkQuery);
+
+    if ($checkResult->num_rows > 0) {
+        echo "Uživatel s tímto jménem nebo emailem již existuje.";
+    } else {
+        // Nové ID pro vložení
+        $new_id = getNewId($conn);
+
+        // Datum registrace
+        $regdate = date("Y-m-d H:i:s");
+
+        // Zpracování a vložení dat do databáze
+        $sql = "INSERT INTO users (id, name, realname, password, email, regdate, address) 
+            VALUES ('$new_id', '$name', '$realname', '$password', '$email', '$regdate', '$ipAddress')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Registrace úspěšná";
+
+            // Získání dat z databáze pro kontrolu
+            $selectQuery = "SELECT * FROM users WHERE name='$name'";
+            $result = $conn->query($selectQuery);
+
+            if ($result->num_rows > 0) {
+                // Výpis nebo zpracování dat z databáze
+                while ($row = $result->fetch_assoc()) {
+                    echo "ID: " . $row["id"] . "<br>";
+                    echo "Name: " . $row["name"] . "<br>";
+                    echo "Realname: " . $row["realname"] . "<br>";
+                    echo "Email: " . $row["email"] . "<br>";
+                    echo "Regdate: " . $row["regdate"] . "<br>";
+                    echo "IP Address: " . $row["address"] . "<br>";
+                }
+            } else {
+                echo "Žádná data nebyla nalezena v databázi.";
+            }
+        } else {
+            echo "Chyba při registraci: " . $conn->error;
+        }
+    }
 } else {
     include "../RegistraceAPrihlaseni/registrace.html"; // Jinak zobrazit obsah souboru nwms.html
 }
 
 // Uzavření připojení k databázi
 $conn->close();
+
+// Funkce pro získání nového ID
+function getNewId($connection) {
+    $result = $connection->query("SELECT MAX(id) AS max_id FROM users");
+    $row = $result->fetch_assoc();
+    $last_id = $row['max_id'];
+    return $last_id + 1;
+}
 ?>
--- Stránky/PHP/registrace.php
+++ Stránky/PHP/registrace.php
@@ -4,50 +4,61 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Funkce pro získání nového ID
+function getNewId($connection) {
+    $result = $connection->query("SELECT MAX(id) AS max_id FROM users");
+    $row = $result->fetch_assoc();
+    $last_id = $row['max_id'];
+    return $last_id !== null ? $last_id + 1 : 1;
+}
+
 // Kontrola, zda jsou data POST metody dostupná
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["name"], $_POST["realname"], $_POST["password"], $_POST["address"], $_POST["email"])) {
-    $name = $conn->real_escape_string($_POST["name"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["realname"], $_POST["password"], $_POST["address"], $_POST["email"])) {
     $realname = $conn->real_escape_string($_POST["realname"]);
+    $name = strtolower($realname); // Uložení jména jako lowercase
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
     $address = $conn->real_escape_string($_POST["address"]);
     $email = $conn->real_escape_string($_POST["email"]);
 
-    // Získání posledního použitého ID
-    $result = $conn->query("SELECT MAX(id) AS max_id FROM users");
-    $row = $result->fetch_assoc();
-    $last_id = $row['max_id'];
+    // Kontrola, zda uživatel s daným jménem nebo emailem již existuje
+    $checkQuery = "SELECT * FROM users WHERE LOWER(name) = LOWER('$name') OR email = '$email'";
+    $checkResult = $conn->query($checkQuery);
 
-    // Nové ID pro vložení
-    $new_id = $last_id + 1;
+    if ($checkResult->num_rows > 0) {
+        echo "Uživatel s tímto jménem nebo emailem již existuje.";
+    } else {
+        // Získání nového ID pro vložení
+        $new_id = getNewId($conn);
 
-    // Datum registrace
-    $regdate = date("Y-m-d H:i:s");
+        // Datum registrace
+        $regdate = date("Y-m-d H:i:s");
 
-    // Zpracování a vložení dat do databáze
-    $sql = "INSERT INTO users (id, name, realname, password, address, email, regdate) VALUES ('$new_id', '$name', '$realname', '$password', '$address', '$email', '$regdate')";
+        // Zpracování a vložení dat do databáze
+        $sql = "INSERT INTO users (id, name, realname, password, address, email, regdate) VALUES ('$new_id', '$name', '$realname', '$password', '$address', '$email', '$regdate')";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Registrace úspěšná";
+        if ($conn->query($sql) === TRUE) {
+            echo "Registrace úspěšná";
 
-        // Získání dat z databáze pro kontrolu
-        $selectQuery = "SELECT * FROM users WHERE name='$name'";
-        $result = $conn->query($selectQuery);
+            // Získání dat z databáze pro kontrolu
+            $selectQuery = "SELECT * FROM users WHERE name='$name'";
+            $result = $conn->query($selectQuery);
 
-        if ($result->num_rows > 0) {
-            // Výpis nebo zpracování dat z databáze
-            while ($row = $result->fetch_assoc()) {
-                echo "ID: " . $row["id"] . "<br>";
-                echo "Name: " . $row["name"] . "<br>";
-                echo "Realname: " . $row["realname"] . "<br>";
-                echo "Address: " . $row["address"] . "<br>";
-                echo "Email: " . $row["email"] . "<br>";
-                echo "Regdate: " . $row["regdate"] . "<br>";
+            if ($result->num_rows > 0) {
+                // Výpis nebo zpracování dat z databáze
+                while ($row = $result->fetch_assoc()) {
+                    echo "ID: " . $row["id"] . "<br>";
+                    echo "Name: " . $row["name"] . "<br>";
+                    echo "Realname: " . $row["realname"] . "<br>";
+                    echo "Address: " . $row["address"] . "<br>";
+                    echo "Email: " . $row["email"] . "<br>";
+                    echo "Regdate: " . $row["regdate"] . "<br>";
+                }
+            } else {
+                echo "Žádná data nebyla nalezena v databázi.";
             }
         } else {
-            echo "Žádná data nebyla nalezena v databázi.";
+            echo "Chyba při registraci: " . $conn->error;
         }
-    } else {
-        echo "Chyba při registraci: " . $conn->error;
     }
 } else {
     echo "Data pro registraci nebyla poskytnuta.";
