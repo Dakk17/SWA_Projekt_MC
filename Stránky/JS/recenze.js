0433a04a24204a759b9302f8e890b899f18fb98b
VisikLost <106622121+VisikLost@users.noreply.github.com>
2024-03-11 07:35:33 +0100

    Add files via upload

--- Stránky/JS/recenze.js
+++ Stránky/JS/recenze.js
@@ -1,18 +1,16 @@
-// Přidání funkcionality pro přepínání recenzí
+     // Přidání funkcionality pro přepínání recenzí
 const reviewGallery = document.getElementById('reviewGallery');
 const prevButton = document.getElementById('prevButton');
 const nextButton = document.getElementById('nextButton');
+const reviewItem1 = document.querySelector('.reviewItem1');
+const reviewItem2 = document.querySelector('.reviewItem2');
 let currentIndex = 0;
 
-prevButton.addEventListener('click', () => changeReview(-1));
-nextButton.addEventListener('click', () => changeReview(1));
-
-function changeReview(direction) {
-    const reviewItems = document.querySelectorAll('.reviewItem');
-    const totalReviews = reviewItems.length;
-
-    currentIndex = (currentIndex + direction + totalReviews) % totalReviews;
-
-    const displacement = -currentIndex * 320; // Šířka recenze + odstup
-    reviewGallery.style.transform = `translateX(${displacement}px)`;
-}
+prevButton.addEventListener('click', function(){
+    reviewItem1.style.display = "Block"
+    reviewItem2.style.display = "None"
+});
+nextButton.addEventListener('click', function(){
+    reviewItem1.style.display = "None"
+    reviewItem2.style.display = "Block"
+});
